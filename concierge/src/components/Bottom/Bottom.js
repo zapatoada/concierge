f5bee02351055f85acf341a47be3c8c9b021fe04
zapatoada <32368237+zapatoada@users.noreply.github.com>
2019-05-14 21:19:32 -0400

    api based tabs

--- concierge/src/components/Bottom/Bottom.js
+++ concierge/src/components/Bottom/Bottom.js
@@ -5,8 +5,12 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import CardTravelIcon from '@material-ui/icons/CardTravel';
 import NewReleasesIcon from '@material-ui/icons/NewReleases';
-import NaturePeopleIcon from '@material-ui/icons/NaturePeople';
 import LocalDiningIcon from '@material-ui/icons/LocalDining';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
 import Typography from '@material-ui/core/Typography';
 import Item from '../Item'
 import "./Bottom.css";
@@ -14,6 +18,17 @@ import "./Bottom.css";
 
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 // import PersonPinIcon from '@material-ui/icons/PersonPin';
+
+
+const categoryIconMap = {
+  "restaurants within 1 block": <LocalDiningIcon />,
+  "breakfast": <FreeBreakfastIcon />,
+  "my picks restaurants": <NewReleasesIcon />,
+  "things to do close by": <PinDropIcon />,
+  "music": <MusicNoteIcon />,
+  "beer": <LocalDrinkIcon />,
+  "museums": <AccountBalanceIcon />
+}
 
 const styles = {
   root: {
@@ -27,7 +42,8 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-start',
     overflow: 'auto',
-    paddingRight: '1rem'
+    paddingRight: '1rem',
+    height: 'calc(100vh - 235px)'
   },
   spacer: {
     opacity: 0,
@@ -58,20 +74,16 @@ class Bottom extends React.Component {
 
   handleChange = (event, value) => {
     window.appInsights && window.appInsights.trackPageView(`tab-${value}-${event.target.innerText}`);
+    Array.from(document.querySelectorAll('.scroll')).forEach(e => e.scrollLeft = 0);
     this.setState({ value });
   };
 
   render() {
     const { classes, categories } = this.props;
     const { value } = this.state;
-    const dining = categories.filter(p => p.Name === 'My Picks Restaurants')[0];
-    const todo = categories.filter(p => p.Name === 'Things to Do Close By')[0];
-    const museums= categories.filter(p => p.Name === 'Museums')[0];
-
+    const currentTab = value > 0 ? categories[value - 1] : null;
 
     return (
-      //   <Paper square className={classes.root}>
-
       <div className="classes.root">
         <div>
           {value === 0 && <TabContainer><div className='homeBackground'>
@@ -85,56 +97,23 @@ class Bottom extends React.Component {
             </Typography>
           </div>
           </TabContainer>}
-          {value === 1 && <TabContainer>
-            <div className='eventsBackground'>
+
+          {currentTab && value > 0 && <TabContainer>
+            <div className="eventsBackground">
               <div className={classes.page}>
                 <Typography variant="h3" className={classes.pageTitle}>
-                  Things To Do Close By
-              </Typography>
-                <div className={classes.scroll}>
-                  {todo && todo.items && todo.items.map(p => (
-                    <Item item={p} key={p.RowKey} />
+                  {currentTab.Name}
+                </Typography>
+                <div className={`${classes.scroll} scroll`}>
+                  {currentTab.items && currentTab.items.map(p => (
+                    <Item item={p} ke741y={p.RowKey} />
                   ))}
-                  {/* stupid hack because css is dumb */}
                   <div className={classes.spacer} />
                 </div>
               </div>
             </div>
           </TabContainer>}
-          {value === 2 &&
-            <TabContainer>
-              <div className='diningBackground'>
-                <div className={classes.page}>
-                  <Typography variant="h3" className={classes.pageTitle}>
-                    My Picks
-                  </Typography>
-                  <div className={classes.scroll}>
-                    {dining && dining.items && dining.items.map(p => (
-                      <Item item={p} key={p.RowKey} />
-                    ))}
-                    {/* stupid hack because css is dumb */}
-                    <div className={classes.spacer} />
-                  </div>
-                </div>
-              </div>
-            </TabContainer>}
-          {value === 3 &&
-            <TabContainer>
-              <div className='attractionsBackground'>
-              <div className={classes.page}>
-              <Typography variant="h4" className={classes.pageTitle}>
-                Museums
-              </Typography>
-              <div className={classes.scroll}>
-                {museums && museums.items && museums.items.map(p => (
-                  <Item item={p} key={p.RowKey} />
-                ))}
-                {/* stupid hack because css is dumb */}
-                <div className={classes.spacer} />
-              </div>
-            </div>
-              </div>
-            </TabContainer>}
+
         </div>
 
         <div style={{ position: "fixed", bottom: "0", left: "0", width: "100%", textColor: 'ffffff' }}>
@@ -144,13 +123,12 @@ class Bottom extends React.Component {
             onChange={this.handleChange}
             varient="fullWidth"
             indicatorColor="secondary"
-            textColor="secondary"
+            textColor="secondary">
 
-          >
             <Tab icon={<CardTravelIcon />} label="YOUR VISIT" />
-            <Tab icon={<NewReleasesIcon />} label="THINGS TO DO CLOSE BY" />
-            <Tab icon={<LocalDiningIcon />} label="MY PICKS RESTAURANTS" />
-            <Tab icon={<NaturePeopleIcon />} label="MUSEUMS" />
+            {categories.map(c =>
+              <Tab icon={categoryIconMap[c.Name.toLowerCase()]} label={c.Name.toUpperCase()} key={c.RowKey} />
+            )}
           </Tabs>
         </div>
       </div>
